62c2f52a084c16145fc4b1bb54530667b2411d6c
Tamilselvan A <77477264+TamilselvanAvatar@users.noreply.github.com>
2023-03-16 15:16:54 +0000

    enhanced

--- src/Merge.js
+++ src/Merge.js
@@ -1,36 +1,55 @@
-import React, { useState } from 'react';
-import { ImSpinner10 } from 'react-icons/im'
-import projectinfo from './projectinfo'
+import React, { useState } from "react";
+import { ImSpinner10 } from "react-icons/im";
+import projectinfo from "./projectinfo";
 
 const style = {
-    display: "flex",
-    margin: "2px",
-    boxShadow: "1px 0.5px 1px",
-    border: "1px solid gray",
-    bordeRadius: "12px",
-    backgroundColor: "aliceblue",
-    filter: "blur()",
-    cursor: "pointer",
-    flexDirection: "column",
-}
+  display: "flex",
+  width: "75%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginBottom : "5px",
+  boxShadow: "0.5px 0.5px 4px",
+  border: "0.5px solid gray",
+  borderRadius: "12px",
+  backgroundColor: "#dde7f0",
+  cursor: "pointer",
+  flexDirection: "column",
+};
 
 export default function Merge() {
-    const [component, setComponent] = useState(null)
-    const onClickHandler = (element) => {
-        const com = <React.Suspense fallback={<ImSpinner10 />}>
-            {element.component}
-        </React.Suspense>
-        setComponent(com)
-        window.history.pushState({},"","/next")
-    }
-    const renderComponent = projectinfo.map((el, i) => {
-        return (<div style={style} key={i} onClick={() => onClickHandler(el)}>
-            <h4 style={{textAlign: "center"}}>{el.name}</h4>
-            <p style={{padding:"5px"}}>
-                {el.description}
-            </p>
-        </div>)
-    })
-    return <>{component || renderComponent}</>
-
+  const [component, setComponent] = useState(null);
+  const onClickHandler = (element) => {
+    const com = (
+      <React.Suspense fallback={<ImSpinner10 />}>
+        {element.component}
+      </React.Suspense>
+    );
+    setComponent(com);
+  };
+  const renderComponent = projectinfo.map((el, i) => {
+    const {
+      description: { Aim, Using, Learn, Default },
+    } = el;
+    return (
+      <div style={style} key={i} onClick={() => onClickHandler(el)}>
+        <h4 style={{ textAlign: "center", fontStyle : "italic", color: "GrayText" , fontWeight : "bolder" }}>{el.name}</h4>
+        <div style={{ padding: "1px" , marginLeft : "5px" }}>
+          <p>
+            <b>Aim : </b>
+            {Aim}
+          </p>
+          <p>
+            <b>Used concepts : </b>
+            {Using}
+          </p>
+          <p>
+            <b>Understood : </b>
+            {Learn}
+          </p>
+          <p>{Default}</p>
+        </div>
+      </div>
+    );
+  });
+  return <>{  component  || renderComponent  }</>;
 }
--- src/projectinfo.js
+++ src/projectinfo.js
@@ -1,78 +1,84 @@
-import React from 'react'
-const Animal = React.lazy(() => import('./animals/Animals'))
-const Book = React.lazy(() => import('./books/Books'))
-const BookRefactor = React.lazy(() => import('./books-refactor/Books'))
-const Routing = React.lazy(() => import('./rounting/routing'))
-const StateManagement = React.lazy(() => import('./state-management/state'))
-const Cars = React.lazy(() => import('./cars/cars'))
-const Media = React.lazy(() => import('./media/media.js'))
+import React from "react";
+const Animal = React.lazy(() => import("./animals/Animals"));
+const Book = React.lazy(() => import("./books/Books"));
+const BookRefactor = React.lazy(() => import("./books-refactor/Books"));
+const Routing = React.lazy(() => import("./rounting/routing"));
+const StateManagement = React.lazy(() => import("./state-management/state"));
+const Cars = React.lazy(() => import("./cars/cars"));
+const Media = React.lazy(() => import("./media/media.js"));
 const project = [
-    {
-        name: "Animal",
-        component : <Animal />,
-        description: `
-    Aim - Render the 'Animal' images <br>
-    Using 'useSate' Hook and props <br>
-    Learn - General JSX, State, Rerender
-    `
+  {
+    name: "Animal",
+    component: <Animal />,
+    description: {
+      Aim: `Render the 'Animal' images`,
+      Using: `'useSate' Hook and props`,
+      Learn: `General concept of JSX, State, Rerender of functional component`,
+      Default :""
     },
-    {
-        name: "Book",
-        component : <Book />,
-        description: `
-    Aim - Props communication and some familiariation with axios 
-    Using Hook, Props, Axios
-    Introduced - fake-json server , add a 'run script' in package.json
-    Create a book.json act as DB
-    Learn - Importance of Spliting the logic as 'Components
-    `
-    },
-    {
-        name: "Book Refactor",
-        component : <BookRefactor />,
-        description: `
-    Aim -  same as Book
-    Instead of Props, used - 'useContext' Hook
-    Learn - Access of state without props (in central management)
-    `
-    },
-    {
-        name: "Routing",
-        component : <Routing />,
-        description: `
-    Aim -  Route through different component
-    Created custom Route and Link component without using library
-    Introducing of 'useReducer'
-    Learn - Custom resusable hook and component
-    `
-    },
-    {
-        name: "State Management",
-        component : <StateManagement />,
-        description: `
-    Aim -  Managing State 
-    Using Redux toolkit  
-    Learn - Slice -> Action Creators -> Store -> Provider
-    `
-    },
-    {
-        name: "Cars",
-        component : <Cars />,
-        description: `
-    Aim -  Add, Fetch, Remove cars with combined components
-    Using Redux toolkit  
-    Learn - Slice -> Action Creators -> Reducer -> Store -> Provider
-    `
-    },
-    {
-        name: "Media",
-        component : <Media />,
-        description: `
-    Aim -  Add, Fetch, Remove media with combined components
-    Using Redux toolkit, createAsynThunk, createApi  
-    Learn - Query -> Tag system - for re-request
-    `
-    },
-]
+  },
+  {
+    name: "Book",
+    component: <Book />,
+    description:{
+        Aim : `Props communication and some familiariation with axios`,
+        Using : `Using Hook, Props, Axios`,
+        Learn : `Importance of Spliting the logic as 'Components'`,
+        Default : `Introduced - fake-json server , add a 'run script' in package.json
+        Create a book.json act as DB`
+    } 
 
-export default project
+  },
+  {
+    name: "Book Refactor",
+    component: <BookRefactor />,
+    description: {
+        Aim : `Props communication and some familiariation with axios`,
+        Using : `Instead of Props, used - 'useContext' Hook`,
+        Learn : `Access of state without props (in central management)`,
+        Default : ""
+    }
+  },
+  {
+    name: "Routing",
+    component: <Routing />,
+    description:{
+        Aim : `Route through different component`,
+        Using : `Created custom Route and Link component without using library`,
+        Learn : `Custom resusable hook and component`,
+        Default : "Introducing of 'useReducer'"
+    } 
+  },
+  {
+    name: "State Management",
+    component: <StateManagement />,
+    description: {
+        Aim : `Managing State `,
+        Using : `Using Redux toolkit`,
+        Learn : `Slice -> Action Creators -> Store -> Provider`,
+        Default : ""
+    } 
+  },
+  {
+    name: "Cars",
+    component: <Cars />,
+    description: {
+        Aim : `Add, Fetch, Remove cars with combined components `,
+        Using : `Using Redux toolkit`,
+        Learn : `Slice -> Action Creators -> Reducer -> Store -> Provider`,
+        Default : ""
+    } ,
+  },
+  {
+    name: "Media",
+    component: <Media />,
+    description: {
+        Aim : `Add, Fetch, Remove cars with combined components `,
+        Using : `Using Redux toolkit, createAsynThunk, createApi `,
+        Learn : `Query -> Tag system - for re-request`,
+        Default : ""
+    } ,
+  },
+];
+
+export default project;
